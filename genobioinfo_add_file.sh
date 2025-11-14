e5a90ced0409b2731fe7c8d3839abad75d89b49e
qizhengyang <qizhengyang17@gmail.com>
2025-11-14 17:36:25 +0100

    update genobioinfo

---
+++ genobioinfo_add_file.sh
@@ -0,0 +1,13 @@
+# vim
+cp -p ~/.vimrc genobioinfo/vim/vimrc
+
+# bash
+cp -p ~/.bashrc genobioinfo/bash/bashrc
+cp -p ~/.env.sh genobioinfo/bash/env.sh
+
+# tmux
+cp -p ~/.tmux.conf genobioinfo/tmux/tmux.conf
+cp -p ~/.tmux.conf.local genobioinfo/tmux/tmux.conf.local
+
+# nvim
+cp -rp ~/.config/nvim genobioinfo
