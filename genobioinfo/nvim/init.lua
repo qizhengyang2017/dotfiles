54d2e2c36ab7b4abe3190f021e4cbea18fccabeb
qizhengyang <qizhengyang17@gmail.com>
2025-11-17 10:34:23 +0100

    genobioinfo:nvim clipboard setting

--- genobioinfo/nvim/init.lua
+++ genobioinfo/nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+vim.opt.wrap = true
+
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
