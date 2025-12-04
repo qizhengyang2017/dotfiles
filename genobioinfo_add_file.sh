2cd5610ba465621201e6882a421dbc337a92e80e
qizhengyang <qizhengyang17@gmail.com>
2025-12-04 16:39:36 +0100

    genobioinfo add bash history

--- genobioinfo_add_file.sh
+++ genobioinfo_add_file.sh
@@ -5,6 +5,9 @@ cp -p ~/.vimrc genobioinfo/vim/vimrc
 cp -p ~/.bashrc genobioinfo/bash/bashrc
 cp -p ~/.env.sh genobioinfo/bash/env.sh
 
+# 2025-12-04
+cp -p ~/.bash_history genobioinfo/bash/bash_history
+
 # tmux
 cp -p ~/.tmux.conf genobioinfo/tmux/tmux.conf
 cp -p ~/.tmux.conf.local genobioinfo/tmux/tmux.conf.local
